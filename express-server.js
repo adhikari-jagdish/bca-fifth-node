691987aad3c00fb759100ddf866048e7e7275a8d
Jagadish <adhikari.jagdish5200@gmail.com>
2025-06-01 09:26:27 +0545

    Minor changes

--- express-server.js
+++ express-server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 const PORT = 3001;
 
@@ -16,6 +20,24 @@ app.get("/about",
     (req, res) => {
         //res.send("This is about page");
         res.json({ name: "Ravi", age: 25 });
+    }
+)
+
+app.get("/search",
+
+    (req, res) => {
+        const requestParams = req.query;
+        console.log(requestParams);
+        res.send("This is search page");
+    }
+)
+
+app.post("/register",
+
+    (req, res) => {
+        const requestBody = req.body;
+        console.log(requestBody);
+        res.send("This is register page");
     }
 )
 
